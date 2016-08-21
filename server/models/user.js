88c8e7299991b5a207bc465b054426d5931f8d47
Gautam Vij <gggautamfog@gmail.com>
2016-08-21 20:54:08 +0530

    dbfeature having some bug

---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+module.exports = function(sequelize, DataTypes) {
+  "use strict";
+
+  var User = sequelize.define("User", {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  });
+
+  return User;
+};
