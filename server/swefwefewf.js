37be75e00d91377890fefb8ba313226cbb67b662
Gautam Vij <gggautamfog@gmail.com>
2016-08-21 20:55:01 +0530

    Revert "Revert "dbfeature having some bug""
    This reverts commit ff20ec06560bc6f825a2297348556964e90d7948.

--- server/swefwefewf.js
+++ server/swefwefewf.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
@@ -7,50 +8,60 @@ app.use(bodyParser.urlencoded({
  extended: true
 }));
 
+
+
 const passport = require('passport');
 const LocalStrategy   = require('passport-local').Strategy;
 app.use(express.static(__dirname  + '/../src'));
-
+app.use(bodyParser.json());
 app.use (require('cookie-parser')());
 
 app.use(session({
    secret: "myloveishate",
    resave: true,
    saveUninitialized: true
-})); 
+}));
+
+
+var models = require('./models/');
+
+app.use(passport.initialize());
+//app.use(passport.session());
+
 
 app.set('views', __dirname + '/../');
 app.engine('html', ejs.renderFile);
 
-// loading models for now its only User
-var models = require('./models/');
+require('./routes')(app);
 
-app.use(passport.initialize());
-app.use(passport.session());
-// getting variable for user model
-var User = models.User;
-
-
-
-
-//configuring passport
 passport.serializeUser(function(user, done) {
-
-        done(null, user.id);
-    });
-
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-        done(err, user);
-    }).then (function(user){
-    console.log(user.id + "is the user here ");
-    console.log("This step is printed with the id 1 ");
-  	});
+  console.log("yahan user aa gya " + user.id );
+  done(null, user.id);
 });
 
 
-passport.use('local-login', new LocalStrategy(
-    function(username, password, done) {
+
+
+passport.deserializeUser(function(id, done) {
+  console.log("here is the deserializeUser stuff " + id );
+  models.User.findById(id,function(err, user) {
+    console.log("This is the error in done " + err);
+    console.log("This si the user in the done " + user.id);
+    done(err, user);
+  })
+  .then (function(user){
+    console.log(user.id + "is the user here ");
+    console.log("done executed");
+  });
+
+});
+
+
+
+
+//done callback returns back to authenticate 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
     console.log(username);
     console.log(password);
     models.User.find({ where: { username: username }}).then(function(user) {
@@ -67,12 +78,31 @@ passport.use('local-login', new LocalStrategy(
       done(err);
     });
   }
-    ));
+));
 
-// injecting passport to the routes
-require('./routes')(app,passport);
+app.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    // Redirect if it fails
+    if (!user) { return res.redirect('/login'); }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      // Redirect if it succeeds
+      return res.redirect('/users/' + user.username);
+    });
+  })(req, res, next);
+});
 
-// initiating server
+/*
+app.post('/login', 
+	passport.authenticate('local', 
+		{ 
+			successRedirect: '/abc', 
+			failureRedirect: '/' 
+		})
+	);
+*/
+
 models.sequelize.sync({
 	force: true,
 	logging: console.log
@@ -81,6 +111,8 @@ models.sequelize.sync({
     models.User.create({username: "gautam", password : "hello"}).then(function(task){
       task.save();
     });
+
+   
 
 		var server = app.listen(8080, function () {
 		    var host = server.address().address;
@@ -91,8 +123,6 @@ models.sequelize.sync({
 	.catch(function(error){
 		console.log(error);
 });
-
-
 
 
 
